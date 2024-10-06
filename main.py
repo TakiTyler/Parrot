36e14b767cff26b59a64f3a02084affd7b6d1142
adarsharun99 <adarsharun99@gmail.com>
2024-10-06 03:18:22 -0400

    added functions

--- main.py
+++ main.py
@@ -10,37 +10,39 @@ apikey = 'tbaNhfKLNJXDNShVRW7gVZ0RrjOErVhG6C5cBgH_EL75'
 STTurl = 'https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/094f1f6b-7a40-412f-838c-649a9dff7f76'
 STTapikey = 'KSxHhEgPDTCRaY3D-kN-OAwPHlFrwJYl__sP6xtd1tRA'
 
-#text to speech section
+# text to speech section
 # ---------------------------------------------------------------------
-# authenticator = IAMAuthenticator(apikey)
-# tts = TextToSpeechV1(authenticator=authenticator)
-# tts.set_service_url(url)
+def TTS():
+    authenticator = IAMAuthenticator(apikey)
+    tts = TextToSpeechV1(authenticator=authenticator)
+    tts.set_service_url(url)
 
-# speaking from given prompt and saving into new audio file
-# with open('./speak.mp3', 'wb') as audio_file:
-#     res = tts.synthesize('Hi Tyler!', accept = 'audio/mp3', voice = 'en-US_AllisonV3Voice').get_result()
-#     audio_file.write(res.content)
+    # speaking from given prompt and saving into new audio file
+    # with open('./speak.mp3', 'wb') as audio_file:
+    #     res = tts.synthesize('Hi Tyler!', accept = 'audio/mp3', voice = 'en-US_AllisonV3Voice').get_result()
+    #     audio_file.write(res.content)
 
-# speaking from txt file and saving into new audio file
-# with open('./prompt.txt', 'r') as file:
-#     text = file.readlines()
-# text = [line.replace('\n', '') for line in text]
-# text = ''.join(str(line) for line in text)
-# with open('./speak.mp3', 'wb') as audio_file:
-#     resFile = tts.synthesize(text, accept = 'audio/mp3', voice = 'en-US_AllisonV3Voice').get_result()
-#     audio_file.write(resFile.content)
+    # speaking from txt file and saving into new audio file
+    with open('./prompt.txt', 'r') as file:
+        text = file.readlines()
+    text = [line.replace('\n', '') for line in text]
+    text = ''.join(str(line) for line in text)
+    with open('./speak.mp3', 'wb') as audio_file:
+        resFile = tts.synthesize(text, accept = 'audio/mp3', voice = 'en-US_AllisonV3Voice').get_result()
+        audio_file.write(resFile.content)
 
 # ---------------------------------------------------------------------
-
 
 #speech to text section
 # ---------------------------------------------------------------------
-# STTauthenticator = IAMAuthenticator(STTapikey)
-# stt = SpeechToTextV1(authenticator=STTauthenticator)
-# stt.set_service_url(STTurl)
-#
-# with open('./listen.mp3', 'rb') as f:
-#     sttres = stt.recognize(audio=f, content_type='audio/mp3', model='en-AU_Telephony').get_result()
-# sttText = sttres['results'][0]['alternatives'][0]['transcript']
-# print(sttText)
+def SST():
+    STTauthenticator = IAMAuthenticator(STTapikey)
+    stt = SpeechToTextV1(authenticator=STTauthenticator)
+    stt.set_service_url(STTurl)
+
+    with open('./listen.mp3', 'rb') as f:
+        sttres = stt.recognize(audio=f, content_type='audio/mp3', model='en-AU_Telephony').get_result()
+    sttText = sttres['results'][0]['alternatives'][0]['transcript']
+    print(sttText)
+
 # ---------------------------------------------------------------------
