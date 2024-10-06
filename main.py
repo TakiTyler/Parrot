a874ee0bc055898023d3bda2cebcc42ce6164b13
TakiTyler <148297273+TakiTyler@users.noreply.github.com>
2024-10-06 07:41:48 -0400

    Close to final product
    Tweaking some code and adding multi-threading, cleaning up the code more

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from ibm_watson import TextToSpeechV1
 from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
-import sys
 from openai import OpenAI
 import keyboard
 import time
@@ -10,39 +9,40 @@ import pyaudio
 import wave
 from pydub import AudioSegment
 from playsound import playsound
-import speech_recognition as sr
-from pyfirmata import Arduino, util, SERVO
+from pyfirmata2 import Arduino
+import threading
 
-#text to speech api/url
+# text to speech api/url
 url = 'https://api.au-syd.text-to-speech.watson.cloud.ibm.com/instances/535507f1-3e96-48ee-8ef1-860a70aa6888'
 apikey = 'tbaNhfKLNJXDNShVRW7gVZ0RrjOErVhG6C5cBgH_EL75'
 
-#speech to test api/url
+# speech to test api/url
 STTurl = 'https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/094f1f6b-7a40-412f-838c-649a9dff7f76'
 STTapikey = 'KSxHhEgPDTCRaY3D-kN-OAwPHlFrwJYl__sP6xtd1tRA'
 
-#set up board and two servos
+# set up board and two servos
 board = Arduino('COM3')
 
 head = board.get_pin('d:9:s')
 beak = board.get_pin('d:8:s')
 
 
-
-
 def mouthOpen():
+    head.write(0)
+    beak.write(180)
+
+
+def mouthClose():
     head.write(180)
     beak.write(0)
 
-def mouthClose():
-    head.write(0)
-    beak.write(180)
 
 def talking():
     mouthOpen()
     time.sleep(1.25)
     mouthClose()
     time.sleep(1.25)
+
 
 def listenToSpeech():
     buffer_frames = 3200
@@ -59,7 +59,7 @@ def listenToSpeech():
         frames_per_buffer=buffer_frames
     )
 
-    print("NOW RECORDING")
+    print("Ask Polly a Question!")
 
     # Calculating how long we record for (multiplying the buffer_frames gives us the time in seconds)
     seconds = int(rates / buffer_frames * 5)
@@ -71,9 +71,6 @@ def listenToSpeech():
     for i in range(0, seconds):
         data = stream.read(buffer_frames)
         frames.append(data)
-        if keyboard.is_pressed('c'):
-            print("Ended early")
-            break
 
     # Ending the stream
     stream.stop_stream()
@@ -90,6 +87,7 @@ def listenToSpeech():
 
     return
 
+
 # text to speech section
 # ---------------------------------------------------------------------
 def TTS(pollyInput):
@@ -99,7 +97,7 @@ def TTS(pollyInput):
 
     # speaking from given prompt and saving into new audio file
     with open('./speak.wav', 'wb') as audio_file:
-        res = tts.synthesize(pollyInput, accept = 'audio/wav', voice = 'en-US_AllisonV3Voice').get_result()
+        res = tts.synthesize(pollyInput, accept='audio/wav', voice='en-US_AllisonV3Voice').get_result()
         audio_file.write(res.content)
 
     # speaking from txt file and saving into new audio file
@@ -111,9 +109,9 @@ def TTS(pollyInput):
     #     resFile = tts.synthesize(text, accept = 'audio/mp3', voice = 'en-US_AllisonV3Voice').get_result()
     #     audio_file.write(resFile.content)
 
-# ---------------------------------------------------------------------
 
-#speech to text section
+# ---------------------------------------------------------------------
+# speech to text section
 # ---------------------------------------------------------------------
 def STT():
     STTauthenticator = IAMAuthenticator(STTapikey)
@@ -141,6 +139,7 @@ def initializePolly():
 
     return messages
 
+
 # Function for sending the user's question to the openai API
 def askPolly(messages, userInput):
     addMessages = {"role": "user", "content": userInput}
@@ -161,6 +160,7 @@ def askPolly(messages, userInput):
     print("\nRESPONSE BELOW\n")
 
     return new_response
+
 
 # Grabbing the audio file to modify
 def increasePitch():
@@ -183,15 +183,12 @@ def increasePitch():
 
     return
 
-# audio is played from computer
 
-# at the same time, the parrot starts to move its mouth
-
-# figure out how to start the arduino when we get the audio file
+def talkTime(number):
+    for i in range(number):
+        talking()
 
 ## MAIN
-
-print("AI IS NOW OPERATIONAL")
 
 client = OpenAI()
 
@@ -200,7 +197,6 @@ inputMessages = initializePolly()
 print("Press S to start")
 
 continueLoop = 'y'
-
 
 while continueLoop != 'n':
     # loop for detecting the key press of 's' to start
@@ -213,9 +209,6 @@ while continueLoop != 'n':
             break
 
     try:
-
-
-
 
         listenToSpeech()
 
@@ -232,16 +225,20 @@ while continueLoop != 'n':
         # increasing the pitch of the audio to make it sound more "bird-like"
         increasePitch()
 
+        t1 = threading.Thread(target=playsound(r'C:\Users\takim\OneDrive\Documents\GitHub\Parrot\actuallySpeak.wav'))
+        t2 = threading.Thread(target=talkTime(6))
+
         # Automatically play the sound
-        for x in range(6):
-            talking()
-        playsound(r'C:\Users\takim\OneDrive\Documents\GitHub\Parrot\actuallySpeak.wav')
+        # for x in range(6):
+        #     talking()
 
         # Asking the user if they would like to ask polly another question
         continueLoop = input("Would you like to ask another question? (y/n): ")
 
         # Ensuring either y or Y works
         continueLoop = continueLoop.lower()
+
+        os.remove('actuallySpeak.wav')
     except:
         # Catch-all error in case anything goes wrong
         pollyError = "I couldn't quite hear you, squawk! Please ask me again."
@@ -250,16 +247,14 @@ while continueLoop != 'n':
         TTS(pollyInput=pollyError)
 
         increasePitch()
+        playsound(r'C:\Users\takim\OneDrive\Documents\GitHub\Parrot\actuallySpeak.wav')
         for x in range(2):
             talking()
-        playsound(r'C:\Users\takim\OneDrive\Documents\GitHub\Parrot\actuallySpeak.wav')
 
+        # Continuing loop
         continueLoop = input("Would you like to ask another question? (y/n): ")
 
         continueLoop = continueLoop.lower()
+
+        os.remove('actuallySpeak.wav')
         continue
-
-
-# Make the listening to audio process smoother
-    # After a certain amount of time, stop the recording
-    # Up to a maximum of... 10 seconds?
