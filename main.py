54573e0cc5f36eb621ed99c44e4078c3fc5c85fc
TakiTyler <takimototyler@gmail.com>
2024-12-09 15:46:32 -0500

    Removing unnecessary files
    A few files got left over from us uploading the final version, removed those to make the git cleaner

--- main.py
+++ main.py
@@ -2,22 +2,47 @@ from ibm_watson import TextToSpeechV1
 from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
-import sys
 from openai import OpenAI
 import keyboard
 import time
 import pyaudio
 import wave
 from pydub import AudioSegment
+from playsound import playsound
+from pyfirmata2 import Arduino
+import threading
+import pygame
 
-#text to speech api/url
+# text to speech api/url
 url = 'https://api.au-syd.text-to-speech.watson.cloud.ibm.com/instances/535507f1-3e96-48ee-8ef1-860a70aa6888'
 apikey = 'tbaNhfKLNJXDNShVRW7gVZ0RrjOErVhG6C5cBgH_EL75'
 
-#speech to test api/url
+# speech to test api/url
 STTurl = 'https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/094f1f6b-7a40-412f-838c-649a9dff7f76'
 STTapikey = 'KSxHhEgPDTCRaY3D-kN-OAwPHlFrwJYl__sP6xtd1tRA'
 
+# set up board and two servos
+board = Arduino('COM3')
+
+head = board.get_pin('d:9:s')
+beak = board.get_pin('d:8:s')
+
+
+def mouthOpen():
+    head.write(0)
+    beak.write(180)
+
+
+def mouthClose():
+    head.write(180)
+    beak.write(0)
+
+
+def talking():
+    mouthOpen()
+    time.sleep(1.25)
+    mouthClose()
+    time.sleep(1.25)
 
 
 def listenToSpeech():
@@ -35,10 +60,10 @@ def listenToSpeech():
         frames_per_buffer=buffer_frames
     )
 
-    print("NOW RECORDING")
+    print("Ask Polly a Question!")
 
     # Calculating how long we record for (multiplying the buffer_frames gives us the time in seconds)
-    seconds = int(rates / buffer_frames * 5)
+    seconds = int(rates / buffer_frames * 3)
 
     # Frames will store the audio data as we are looping, eventually being turned into a .wav file
     frames = []
@@ -47,9 +72,6 @@ def listenToSpeech():
     for i in range(0, seconds):
         data = stream.read(buffer_frames)
         frames.append(data)
-        if keyboard.is_pressed('c'):
-            print("Ended early")
-            break
 
     # Ending the stream
     stream.stop_stream()
@@ -66,6 +88,7 @@ def listenToSpeech():
 
     return
 
+
 # text to speech section
 # ---------------------------------------------------------------------
 def TTS(pollyInput):
@@ -75,7 +98,7 @@ def TTS(pollyInput):
 
     # speaking from given prompt and saving into new audio file
     with open('./speak.wav', 'wb') as audio_file:
-        res = tts.synthesize(pollyInput, accept = 'audio/wav', voice = 'en-US_AllisonV3Voice').get_result()
+        res = tts.synthesize(pollyInput, accept='audio/wav', voice='en-US_AllisonV3Voice').get_result()
         audio_file.write(res.content)
 
     # speaking from txt file and saving into new audio file
@@ -87,9 +110,9 @@ def TTS(pollyInput):
     #     resFile = tts.synthesize(text, accept = 'audio/mp3', voice = 'en-US_AllisonV3Voice').get_result()
     #     audio_file.write(resFile.content)
 
-# ---------------------------------------------------------------------
 
-#speech to text section
+# ---------------------------------------------------------------------
+# speech to text section
 # ---------------------------------------------------------------------
 def STT():
     STTauthenticator = IAMAuthenticator(STTapikey)
@@ -111,10 +134,13 @@ def initializePolly():
                                       "you are a parrot. You cannot be too formal when speaking and must add a "
                                       "'squawk' at the start of your responses. Additionally, your language should be "
                                       "similar to that of a pirate, but still output coherent sentences, "
-                                      "without being mean. Very rarely mention crackers"}
+                                      "without being mean. Very rarely mention crackers. Limit your responses"
+                                      "to four sentences. Squawk does not count as a sentence. Pause after each squawk."
+                                      "Only one squawk per response."}
     ]
 
     return messages
+
 
 # Function for sending the user's question to the openai API
 def askPolly(messages, userInput):
@@ -126,67 +152,118 @@ def askPolly(messages, userInput):
         model="gpt-3.5-turbo",
         messages=messages,
         temperature=0.7,
-        max_tokens=50,
+        max_tokens=100,
     )
 
-    return response
+    new_response = response.choices[0].message.content
+
+    # new_response = new_response.replace("'", "")
+
+    print("\nRESPONSE BELOW\n")
+
+    return new_response
+
 
 # Grabbing the audio file to modify
 def increasePitch():
-    audioFile = AudioSegment.from_wav('listen.wav')
+    audioFile = AudioSegment.from_wav('speak.wav')
 
     # Calculating how much we will increase the "sample rate" by
-    increaseOctave = int(audioFile.frame_rate * 1.5)
+    increaseOctave = int(audioFile.frame_rate * 1.2)
 
     # Audio apparently becomes strange after changing the pitch, this fixes it
     newAudio = audioFile._spawn(audioFile.raw_data, overrides={'frame_rate': increaseOctave})
 
     # Converting sample rate
-    print("WE ARE CONVERTING THE SAMPLE RATE, THIS MIGHT HAVE TO CHANGE DO NOT FORGET")
     newAudio = newAudio.set_frame_rate(44100)  # may change this to 16000 depending on variable: rates
 
-    newAudio.export("read.wav", format="wav")
+    newAudio.export("actuallySpeak.wav", format="wav")
 
     # Deleting the old file
-    os.remove('listen.wav')
+    #os.remove('listen.wav')
+    #os.remove('speak.wav')
 
     return
 
-# audio is played from computer
 
-# at the same time, the parrot starts to move its mouth
-
-# figure out how to start the arduino when we get the audio file
+def talkTime(number):
+    for i in range(number):
+        talking()
 
 ## MAIN
-
-print("AI IS NOW OPERATIONAL")
 
 client = OpenAI()
 
 inputMessages = initializePolly()
 
+mouthClose()
 print("Press S to start")
 
-# loop for detecting the key press of 's' to start
-while True:
+continueLoop = 'y'
+
+while continueLoop != 'n':
+    # loop for detecting the key press of 's' to start
+    while True:
+        try:
+            if keyboard.is_pressed('s'):
+                print("The Parrot is now talking. Have fun!")
+                break  # finishing the loop
+        except:
+            break
+
     try:
-        if keyboard.is_pressed('s'):  # if key 'q' is pressed
-            print("The Parrot is now talking. Have fun!")
-            break  # finishing the loop
+
+        listenToSpeech()
+
+        # Add the audio recording
+        userInput = STT()
+        print(userInput)
+
+        pollyOut = askPolly(messages=inputMessages, userInput=userInput)
+        print(pollyOut)
+
+        # pollyOut = ("Squawk! Eight planets there be in our solar system, arrr! From the closest to the sun, they be Mercury, Venus (Earth's sister!), Earth, Mars, Jupiter, Saturn, Uranus, and Neptune.")
+        TTS(pollyInput=pollyOut)
+
+        # increasing the pitch of the audio to make it sound more "bird-like"
+        increasePitch()
+
+        # t1 = threading.Thread(target=playsound(r'C:\Users\takim\OneDrive\Documents\GitHub\Parrot\actuallySpeak.wav'))
+        # t2 = threading.Thread(target=talkTime(6))
+
+        pygame.mixer.init()
+        pygame.mixer.music.load(r'C:\Users\takim\OneDrive\Documents\GitHub\Parrot\actuallySpeak.wav')
+        pygame.mixer.music.play()
+        # Automatically play the sound
+        for x in range(6):
+            talking()
+        if pygame.mixer.music.get_busy() == False:
+            pygame.mixer.music.stop()
+            file = 'actuallySpeak.wav'
+            os.remove(file)
+        # Asking the user if they would like to ask polly another question
+        continueLoop = input("Would you like to ask another question? (y/n): ")
+
+        # Ensuring either y or Y works
+        continueLoop = continueLoop.lower()
+
+        #os.remove('actuallySpeak.wav')
     except:
-        break
+        # Catch-all error in case anything goes wrong
+        pollyError = "I couldn't quite hear you, squawk! Please ask me again."
 
-listenToSpeech()
+        # Make the parrot say something went wrong
+        TTS(pollyInput=pollyError)
 
-# Add the audio recording
-userInput = STT()
+        increasePitch()
+        playsound(r'C:\Users\takim\OneDrive\Documents\GitHub\Parrot\actuallySpeak.wav')
+        for x in range(2):
+            talking()
 
-pollyOut = askPolly(messages=inputMessages, userInput=userInput)
-print(pollyOut)
+        # Continuing loop
+        continueLoop = input("Would you like to ask another question? (y/n): ")
 
-TTS(pollyInput=pollyOut)
+        continueLoop = continueLoop.lower()
 
-# If listen to speech didn't work, we play an audio that says to try again
-
-increasePitch()
+        #os.remove('actuallySpeak.wav')
+        continue
